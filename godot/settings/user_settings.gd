6d79ca24a5f28d18224e769afc732fc42f240802
Juan Manuel Fernandes dos Santos <juan9794@gmail.com>
2024-09-03 13:44:04 -0300

    Agregados template de proyecto + assets de Tiny Swords

---
+++ godot/settings/user_settings.gd
@@ -0,0 +1,66 @@
+extends Node
+
+signal on_value_change(key, value)
+
+const SECTION = "user"
+const SETTINGS_FILE = "user://settings.cfg"
+
+const MASTERVOLUME = "mastervolume"
+const MUSICVOLUME = "musicvolume"
+const SOUNDVOLUME = "soundvolume"
+const GAME_LANGUAGE = "game_locale"
+
+const AUDIO_BUS_MASTER = "Master"
+const AUDIO_BUS_SOUND = "Sound"
+const AUDIO_BUS_MUSIC = "Music"
+	
+var USER_SETTING_DEFAULTS = {
+	MASTERVOLUME:100,
+	MUSICVOLUME:70,
+	SOUNDVOLUME:100,
+	GAME_LANGUAGE:"es"
+}
+
+var config:ConfigFile
+
+func _ready():
+	config = ConfigFile.new()
+	config.load(SETTINGS_FILE)
+	_configure_audio()
+	_configure_language()
+	
+func set_value(key, value):
+	config.set_value(SECTION, key, value)
+	config.save(SETTINGS_FILE)
+	if key == MASTERVOLUME:
+		_update_volume(MASTERVOLUME, AUDIO_BUS_MASTER)
+	if key == SOUNDVOLUME:
+		_update_volume(SOUNDVOLUME, AUDIO_BUS_SOUND)
+	if key == MUSICVOLUME:
+		_update_volume(MUSICVOLUME, AUDIO_BUS_MUSIC)
+	if key == GAME_LANGUAGE:
+		TranslationServer.set_locale(value)
+	emit_signal("on_value_change", key, value)
+	
+func get_value(key):
+	return config.get_value(SECTION, key, _get_default(key))
+	
+func get_value_with_default(key, default):
+	return config.get_value(SECTION, key, default)
+
+func _get_default(key):
+	if USER_SETTING_DEFAULTS.has(key):
+		return USER_SETTING_DEFAULTS[key]
+	return null
+
+func _configure_audio():
+	_update_volume(MASTERVOLUME, AUDIO_BUS_MASTER)
+	_update_volume(MUSICVOLUME, AUDIO_BUS_MUSIC)
+	_update_volume(SOUNDVOLUME, AUDIO_BUS_SOUND)
+	
+func _update_volume(property, bus):
+	var current = (get_value_with_default(property, USER_SETTING_DEFAULTS[property]) -100) / 2
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index(bus), current)
+
+func _configure_language():
+	TranslationServer.set_locale(get_value_with_default(GAME_LANGUAGE, USER_SETTING_DEFAULTS[GAME_LANGUAGE])) 
