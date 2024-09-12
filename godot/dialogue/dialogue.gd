b6b0497b070f116d5988275d35688dad57f9061f
Juan Manuel Fernandes dos Santos <juan9794@gmail.com>
2024-09-12 13:40:43 -0300

    Agregados await a la documentacion del sistema de dialogos

--- godot/dialogue/dialogue.gd
+++ godot/dialogue/dialogue.gd
@@ -7,6 +7,19 @@ class_name DialogueSystem extends CanvasLayer
 ## [br]
 ## Esta clase no debería instanciarse
 ## directamente.
+## [br][br]
+## [b]Importante: [/b] todos los métodos que muestran texto se pueden
+## utilizar con await para que el código a ejecutar no avance hasta que la
+## línea haya sido leída y avanzada.[br]
+## [i]Ejemplo:[br]
+## Queremos hacer un personaje que diga algo y luego se borre de la escena.
+## Con el siguiente código, no va a ejecutarse el [code]queue_free()[/code] 
+## hasta que el jugador haya leído y avanzado la línea de texto "¡Me voy!".[/i]
+## [codeblock]
+## func escapar():
+##   await Dialogue.show_line("¡Me voy!")
+##   queue_free()
+## [/codeblock]
 
 ## Burbuja de texto donde se mostraran los mensajes
 @onready var text_bubble: TextBubble = $TextBubble
@@ -14,7 +27,7 @@ class_name DialogueSystem extends CanvasLayer
 ## Muestra un dialogo con el texto [param line].[br]
 ## Ejemplo:
 ## [codeblock]
-## Dialogue.show_line("¡Hola mundo!")
+## await Dialogue.show_line("¡Hola mundo!")
 ## [/codeblock]
 func show_line(line: String):
 	text_bubble.appear()
@@ -26,7 +39,7 @@ func show_line(line: String):
 ## Cada linea de dialogo es un elemento del array [param lines].[br]
 ## Ejemplo:
 ## [codeblock]
-## Dialogue.show_multiple_lines(["Hola"], ["¿qué tal?"])
+## await Dialogue.show_multiple_lines(["Hola"], ["¿qué tal?"])
 ## [/codeblock]
 func show_multiple_lines(lines: Array[String]):
 	for line in lines:
@@ -36,7 +49,7 @@ func show_multiple_lines(lines: Array[String]):
 ## con el texto [param line].[br]
 ## Ejemplo:
 ## [codeblock]
-## Dialogue.say_line("Arturo", "¿Cómo dice?")
+## await Dialogue.say_line("Arturo", "¿Cómo dice?")
 ## [/codeblock]
 func say_line(character_name: String, line: String):
 	var complete_line: String =\
@@ -48,7 +61,7 @@ func say_line(character_name: String, line: String):
 ## Cada linea de de dialogo es un elemento del array [param lines].[br]
 ## Ejemplo:
 ## [codeblock]
-## Dialogue.say_multiple_lines("Arturo", ["¿Podría repetirmelo?", "No entiendo mucho inglés"])
+## await Dialogue.say_multiple_lines("Arturo", ["¿Podría repetirmelo?", "No entiendo mucho inglés"])
 ## [/codeblock]
 func say_multiple_lines(character_name: String, lines: Array[String]):
 	for line in lines:
