5423e40bd3fdb063daee727734a9afeac0b98e73
YsaLuna <ysamarotaku30@gmail.com>
2024-09-12 13:29:10 -0500

    jugador puerta

--- godot/assets/Gold Mine/cueva.gd
+++ godot/assets/Gold Mine/cueva.gd
@@ -18,7 +18,4 @@ func _on_body_entered(body):
 		tiempo.start()
 		await (tiempo.timeout)
 		get_tree().change_scene_to_file("res://scenes/levels/Ysa/escena2.tscn")
-		
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+		pass
