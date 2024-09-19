bc0f8e25cda442369efc72d7490f86c5c3297797
Juan Manuel Fernandes dos Santos <juan9794@gmail.com>
2024-09-19 15:29:29 -0300

    Arreglos en pantalla de creditos
    - Se arregló un error que no dejaba abrir ni la pantalla de créditos ni la pantalla principal por una referencia circular entre ellas (ambas exportaban un PackedScene).
    - Se agregó una cámara que sigue al jugador en la pantalla de créditos.
    - Se dejó el botón fijo en la pantalla (agregandolo a un CanvasLayer) y se le pusieron estilos. El botón se usa para volver a la pantalla principal.

--- godot/scenes/levels/Ysa/credits.gd
+++ godot/scenes/levels/Ysa/credits.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-@export var main_menu_scene: PackedScene  # Exporta la escena del menú principal
-@onready var regresar_button = $Button
+@export_file("*.tscn") var main_menu_scene: String  # Exporta la escena del menú principal
+@onready var regresar_button = %BackToMainMenuButton
 
 
 func _ready() -> void:
@@ -9,4 +9,4 @@ func _ready() -> void:
 
 # Función para cambiar a la escena del menú principal
 func _on_regresar_button_pressed() -> void:
-	get_tree().change_scene_to_packed(main_menu_scene)
+	get_tree().change_scene_to_file(main_menu_scene)
