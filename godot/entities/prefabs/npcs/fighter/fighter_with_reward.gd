87fd39ed94ffe0fd67b0cc0ac835b5a54f34a139
Juan Manuel Fernandes dos Santos <juan9794@gmail.com>
2024-09-26 12:12:12 -0300

    Poder configurar dialogos del fighter

--- godot/entities/prefabs/npcs/fighter/fighter_with_reward.gd
+++ godot/entities/prefabs/npcs/fighter/fighter_with_reward.gd
@@ -4,6 +4,8 @@ extends NPC
 @export_category("NPC")
 @export var npc_name: String = "Robert"
 @export_multiline var line: String = "Hola!"
+@export var line_when_player_won: String = "Ganaste!"
+@export var line_when_player_lost: String = "Perdiste!"
 @export_category("Combat")
 @export var in_combat_character: FighterCharacter
 @export_category("Reward")
@@ -18,9 +20,9 @@ func interact_with(player):
 
 	match outcome:
 		CombatScreen.Outcome.PlayerWon:
-			await Dialogue.say_line(npc_name, "Ganaste, toma esto como recompensa")
+			await Dialogue.say_line(npc_name, line_when_player_won)
 			for i in range(0, reward_amount):
 				Inventory.add_item(reward_item)
 			await Dialogue.show_line("Se recibió %d %s" % [reward_amount, Item.name_of(reward_item)])
 		CombatScreen.Outcome.PlayerLost:
-			await Dialogue.say_line(npc_name, "Perdiste, hablame para intentar de nuevo")
+			await Dialogue.say_line(npc_name, line_when_player_lost)
